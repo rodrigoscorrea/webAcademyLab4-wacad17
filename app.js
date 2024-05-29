cdc20a34ef9e2f708b3559d03555172c14d02f5a
rodrigoscorrea <rdorigosc616@gmail.com>
2024-05-29 19:11:00 -0400

    versao inicial

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 /* eslint-env node */
-
 const express = require('express');
+const handlebars = require("express-handlebars")
+
 const app = express();
 
+app.engine("handlebars", handlebars.engine()); //express agr reconhece a engine
+app.set("view engine","handlebars");
+app.set("views", `${__dirname}/views`);
+
+app.use("/img", express.static(`${__dirname}/public/img`))
+app.use("/css", express.static(`${__dirname}/public/css`))
+app.use('/js',express.static(`${__dirname}/public/js`));
 app.get('/', (req, res) => {
-  res.send('Bem vindos ao WebAcademy!!');
+  res.render('index', {
+    layout: false
+  });
 });
 
 app.listen(8080, () => {
